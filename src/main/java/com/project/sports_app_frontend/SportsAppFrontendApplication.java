f0e494d912617f2ca9589e65bc7a82c575d8d548
DagmaraSkorupska <dagmaraskorupska@gmail.com>
2021-01-23 14:16:01 +0100

    first commit

---
+++ src/main/java/com/project/sports_app_frontend/SportsAppFrontendApplication.java
@@ -0,0 +1,13 @@
+package com.project.sports_app_frontend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportsAppFrontendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SportsAppFrontendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/project/sports_app_frontend/SportsAppFrontendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.sports_app_frontend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SportsAppFrontendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
